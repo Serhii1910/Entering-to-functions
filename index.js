aec7d85569bb7238595980c424feb641c16f0bb1
Serhii1910 <vetrovsergey95@gmail.com>
2020-12-26 11:36:21 +0200

    Added triangleChecking func

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // isAdult();
-checkMultiplicity();
+//checkMultiplicity();
+triangleChecking ();
 
 function isAdult () {
 	const age = +prompt('Are you adult? Enter age');
@@ -22,5 +23,17 @@ function checkMultiplicity () {
 		alert('Divisible without residue');
 	} else {
 		alert('Not divisible without residue');
+	}
+}
+
+function triangleChecking () {
+	const side1 = +prompt('Enter lenght of first side');
+	const side2 = +prompt('Enter lenght of second side');
+	const side3 = +prompt('Enter lenght of third side');
+
+	if(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+		alert("Triangle may exist");
+	}	else {
+		alert("Triangle may not exist");
 	}
 }
