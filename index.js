364cf8e5797048911e06f89bb569e7527996f311
Serhii1910 <vetrovsergey95@gmail.com>
2020-12-26 12:29:51 +0200

    Added rombSquare calculation

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 // isAdult();
 //checkMultiplicity();
-triangleChecking ();
+//triangleChecking ();
+rombSquare();
 
-function isAdult () {
+function isAdult() {
 	const age = +prompt('Are you adult? Enter age');
 
 	if (age >= 5) {
@@ -14,7 +15,7 @@ function isAdult () {
 	}
 }
 
-function checkMultiplicity () {
+function checkMultiplicity() {
 	const number1 = +prompt('Enter number 15 or 25');
 	const number2 = +prompt('Enter one number 3, 4 or 5');
 
@@ -26,7 +27,7 @@ function checkMultiplicity () {
 	}
 }
 
-function triangleChecking () {
+function triangleChecking() {
 	const side1 = +prompt('Enter lenght of first side');
 	const side2 = +prompt('Enter lenght of second side');
 	const side3 = +prompt('Enter lenght of third side');
@@ -35,5 +36,25 @@ function triangleChecking () {
 		alert("Triangle may exist");
 	}	else {
 		alert("Triangle may not exist");
+	}
+}
+
+function rombSquare() {
+	const rombLenght = +prompt('Enter lenght (if unknown skip)');
+	const rombHeight = +prompt('Enter height (if unknown skip)');
+	const rombDiagonal1 = +prompt('Enter diagonal1 (if unknown skip)');
+	const rombDiagonal2 = +prompt('Enter diagonal2 (if unknown skip)');
+
+	if(rombLenght === 0 || rombLenght === isNaN || rombHeight === 0 || rombHeight === isNaN) {
+		if(rombDiagonal1 > 0 && rombDiagonal2 > 0) {
+			const rombSquare = (rombDiagonal1 * rombDiagonal2) / 2;
+			alert('Romb square is ' +rombSquare+ ' square units');
+		}
+	}
+	if( rombDiagonal1 === 0 || rombDiagonal1 === isNaN || rombDiagonal2 === 0 || rombDiagonal2 === isNaN) {
+		if(rombLenght > 0 && rombHeight > 0) {
+			const rombSquare = rombLenght * rombHeight;
+			alert('Romb square is ' +rombSquare+ ' square units');
+		}
 	}
 }
